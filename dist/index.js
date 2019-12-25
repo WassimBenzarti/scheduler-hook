1d8d97f10faf7e9bc95ad895260d8c71ae94a2b4
Wassim Benzarti <wass11121996@gmail.com>
2019-12-25 18:19:04 +0000

    hello

--- dist/index.js
+++ dist/index.js
@@ -2,20 +2,23 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var Config_1 = __importDefault(require("./Config"));
+var es6 = __importStar(require("es6-promise"));
+es6.polyfill();
 require("isomorphic-fetch");
-fetch(Config_1.default.NOTIFY_URL_DEV, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-}).then(function (r) { return r.json(); }).then(console.log);
 function schedule(task) {
     return fetch(Config_1.default.NOTIFY_URL_DEV, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(task),
     }).then(function (r) { return r.json(); });
